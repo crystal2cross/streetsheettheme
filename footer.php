85531ed82dbdf40043f4a59565893310a2e28fc5
Crystal <crystal2cross@gmail.com>
2016-12-19 01:28:33 -0800

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Street_Sheet_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'streetsheettheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'streetsheettheme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'streetsheettheme' ), 'streetsheettheme', '<a href="http://www.streetsheet.org/" rel="designer">Crystal Cross</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
