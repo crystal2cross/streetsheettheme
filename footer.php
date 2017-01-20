2442e06f6ce00424574886709410cb907500df42
Crystal <crystal2cross@gmail.com>
2017-01-20 00:09:20 -0800

    Adding footer

--- footer.php
+++ footer.php
@@ -14,9 +14,38 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-                        ©Copyright 2016 Street Sheet. All rights reserved.
-		</div><!-- .site-info -->
+            <div class="streetsheet-footer-bar">
+                <div class="streetsheet-footer-1">
+                    <b>CONTACT US</b>
+                    <p>
+                        Street Sheet<br>
+                        468 Turk Street<br>
+                        San Francisco, CA 94102
+                    </p>
+                    <p>
+                        Call: (415) 346 - 3740 ext 309
+                    </p>
+                </div>
+                <div class="streetsheet-footer-2">
+                    <b>NEWSLETTER</b>
+                    <p>
+                        Sign-up:
+                    </p>
+                </div>
+                <div class="streetsheet-footer-3">
+                    <b>SOCIAL MEDIA</b>
+                    <p>
+                        <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-youtube-square fa-2x" aria-hidden="true"></i>
+                    </p>
+                </div>
+                <hr>
+            </div>
+            <div class="site-info">
+                    ©Copyright 2016 Street Sheet. All rights reserved.
+            </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
