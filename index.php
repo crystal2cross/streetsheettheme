9266af7512a71e95e4eb91b5264e3e6ccbdbcc34
Crystal <crystal2cross@gmail.com>
2017-01-26 03:54:41 -0800

    Switching to fullscreen slider

--- index.php
+++ index.php
@@ -18,13 +18,7 @@ include "wp-load.php"; ?>
             
 		<main id="main" class="site-main" role="main">
                     
-                <?php
-                    // Check if this is the front page and that it is not page 2 or higher
-                    if ( is_front_page() && !is_paged() ) {
-                            // Add featured content slider
-                            get_template_part( 'featureflexslider' );
-                    }
-                    
+                <?php                    
                     $paged = max(1, get_query_var('paged'));
                     
                     $args = array(
